c3c66fc0d7a663b15e580f676bbbe05f287a56cf
web <yenierara06@gmail.com>
2024-06-14 10:56:35 +0700

    expense tracker

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Header from'./components/Header';
+import Footer from'./components/Footer';
+import Transaction from'./components/Transaction';
+import SaldoBox from'./components/SaldoBox';
+import AddTransaction from'./components/AddTransaction';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initTansactions=[
+    {
+        "id":"619941539079",
+        "tanggal":new Date("01 Nov 2021 9:30").getTime(),
+        "keterangan":"Gaji bulanan",
+        "nominal":2500000,
+    },
+    {
+        "id":"619941539070",
+        "tanggal":new Date("01 Nov 2021 9:30").getTime(),
+        "keterangan":"Gaji bulanan",
+        "nominal":7500000,
+    },
+    {
+        "id":"619941539070",
+        "tanggal":new Date("01 Nov 2021 9:30").getTime(),
+        "keterangan":"Belanja bulanan",
+        "nominal":-2500000,
+    },
+];
+
+const App=()=>{
+    const[transactions, setTransaction] = useState(initTansactions);
+    console.log(transactions);
+    //handler untuk menambah data transaction,
+    //akan di-trigger dari komponen AddTransaction
+    const handleTambahTransaction=(data)=>{
+        let newTransactions=[
+            ...transactions,data
+        ];
+        //atur ulang urutan transaction agar tanggal terkecildibagianatas
+        newTransactions.sort((a,b)=>a.tanggal-b.tanggal);
+        setTransaction(newTransactions);
+    }
+    //handler untuk menghapus data transaction dikomponen AddTransaction
+    const handleHapusTransaction=(e)=>{
+        //cari index transaction yang akan dihapus berdasarkan id
+        const result=transactions.findIndex(
+            transaction=>(transaction.id===e.target.id)
+        );
+        //copy transactions karena fungsi splice akan mengubah array asal(mutate)
+        const newTransactions=transactions;
+        newTransactions.splice(result,1);
+        setTransaction([...newTransactions])
+    }
+
+    return(
+    <React.Fragment>
+        <Header/>
+        <SaldoBox transactions={transactions}/>
+        <Transaction transactions={transactions}onHapusTransaction={handleHapusTransaction}/>
+        <AddTransaction onTambahTransaction={handleTambahTransaction}/>
+        <Footer/>
+    </React.Fragment>
+    )
 }
 
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+const Footer=()=>{
+    return(
+        <footer id="footer"className="text-white py-5">
+            <div className="container">
+                <div className="row">
+                    <div className="col text-center">
+                        <p className="lead mb-0">Ilkoom Expense Tracker</p>
+                        <p>Catat setiap pemasukan dan pengeluaranmu ;)</p>       
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
